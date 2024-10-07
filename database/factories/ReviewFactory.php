957eb4e95138fd22fb687cd94c5ad1e486a0d10e
Mihai9088 <mihaitza1058@gmail.com>
2024-10-07 16:25:21 +0200

    modified the factory and seeder logic for better data

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -29,7 +29,7 @@ class ReviewFactory extends Factory
     {
         return $this->state(function (array $attributes) {
             return [
-                'rating' =>fake()->numberBetween(4,5)
+                'rating' => fake()->numberBetween(4, 5)
             ];
         });
     }
@@ -38,7 +38,7 @@ class ReviewFactory extends Factory
     {
         return $this->state(function (array $attributes) {
             return [
-                'rating' => fake()->numberBetween(4,5)
+                'rating' => fake()->numberBetween(2, 3)
             ];
         });
     }
@@ -47,7 +47,7 @@ class ReviewFactory extends Factory
     {
         return $this->state(function (array $attributes) {
             return [
-                'rating' => fake()->numberBetween(4,5)
+                'rating' => fake()->numberBetween(1, 3)
             ];
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,17 +16,17 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Book::factory(33)->create()->each(function ($book) {
-            $numReviews = random_int(5, 30);
+            $numReviews = random_int(5, 10);
             Review::factory()->count($numReviews)->good()->for($book)->create();
         });
 
         Book::factory(33)->create()->each(function ($book) {
-            $numReviews = random_int(5, 30);
+            $numReviews = random_int(5, 10);
             Review::factory()->count($numReviews)->average()->for($book)->create();
         });
 
         Book::factory(34)->create()->each(function ($book) {
-            $numReviews = random_int(5, 30);
+            $numReviews = random_int(5, 10);
             Review::factory()->count($numReviews)->bad()->for($book)->create();
         });
     }
